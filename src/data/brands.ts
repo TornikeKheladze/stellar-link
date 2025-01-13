b551e1a91934e2b192f68bd1993298dc17b1007d
tornikeKheladze <xeladze.tornike@gmail.com>
2025-01-13 17:07:39 +0400

    fix:img path

--- src/data/brands.ts
+++ src/data/brands.ts
@@ -1,32 +1,32 @@
 export const brands = [
   {
     name: "Honeywell",
-    image: "/src/assets/images/brands/honeywell.jpg",
+    image: "/images/brands/honeywell.jpg",
     id: 1,
   },
   {
     name: "Honeywell",
-    image: "/src/assets/images/brands/honeywell.jpg",
+    image: "/images/brands/honeywell.jpg",
     id: 2,
   },
   {
     name: "Honeywell",
-    image: "/src/assets/images/brands/honeywell.jpg",
+    image: "/images/brands/honeywell.jpg",
     id: 3,
   },
   {
     name: "Honeywell",
-    image: "/src/assets/images/brands/honeywell.jpg",
+    image: "/images/brands/honeywell.jpg",
     id: 4,
   },
   {
     name: "Honeywell",
-    image: "/src/assets/images/brands/honeywell.jpg",
+    image: "/images/brands/honeywell.jpg",
     id: 5,
   },
   {
     name: "Honeywell",
-    image: "/src/assets/images/brands/honeywell.jpg",
+    image: "/images/brands/honeywell.jpg",
     id: 6,
   },
 ];
