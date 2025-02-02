db0dc3db97bfb77d8461edbeb109277f14f78437
TornikeKheladze <97169332+TornikeKheladze@users.noreply.github.com>
2025-02-02 08:54:07 -0800

    Update careers.ts

--- src/data/careers.ts
+++ src/data/careers.ts
@@ -6,19 +6,19 @@ export const careers: {
 }[] = [
   {
     id: 1,
-    title: "ვებ დეველოპერი",
+    title: "სუსტი დენების ინჟინერი",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita fugit sint quod assumenda cupiditate sunt, repellendus debitis, amet fugiat veniam, sapiente aliquid exercitationem quaerat.",
     image: "/images/careers/career1.webp",
   },
   {
     id: 2,
-    title: "დიზაინერი",
+    title: "ელექტრო-მემონტაჟე",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita fugit sint quod assumenda cupiditate sunt, repellendus debitis, amet fugiat veniam, sapiente aliquid exercitationem quaerat.",
     image: "/images/careers/career1.webp",
   },
   {
     id: 3,
-    title: "ჟეშტიანჩიკი",
+    title: "საიტ მენეჯერი",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita fugit sint quod assumenda cupiditate sunt, repellendus debitis, amet fugiat veniam, sapiente aliquid exercitationem quaerat.",
     image: "/images/careers/career1.webp",
   },
